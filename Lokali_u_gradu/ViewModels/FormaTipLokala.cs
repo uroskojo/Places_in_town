8cee81d3fb78c1807b95ee53c0e345c6ea1f9ec9
uroskojo <43353383+uroskojo@users.noreply.github.com>
2018-09-19 09:35:20 +0200

    Add files via upload

---
+++ Lokali_u_gradu/ViewModels/FormaTipLokala.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lokali_u_gradu.ViewModels
+{
+    public class FormaTipLokala
+    {
+    }
+}
---
+++ Lokali_u_gradu/ViewModels/MapaViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Lokali_u_gradu.ViewModels
+{
+    public class MapaViewModel
+    {
+        public MapaViewModel()
+        {
+         
+        }
+    }
+}
